4b9ee60390b859448a8da61d2aae8cac53c02759
Daniel <46535297+daniel-slosar@users.noreply.github.com>
2022-09-15 21:07:54 +0200

    First Array Assignment

--- Assignments/Arrays/Arrays_First_Assignment.cpp
+++ Assignments/Arrays/Arrays_First_Assignment.cpp
@@ -1,26 +1,93 @@
+/******************************************************************************
+1. Generate 10 numbers in range [-10,100] into an A array
+2. Sort the numbers in array in ascending and descending order
+3. Find the second smallest and highest number
+*******************************************************************************/
+
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
+int s;
+
+void generate(int array[10])
+{
+    srand(time(0));
+    cout << "Array A[";
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = rand() % 111 - 10;
+        if (i == 9)
+        {
+            cout << array[i];
+        }
+        else {
+            cout << array[i] << ", ";
+        }
+    }
+    cout << "]\n";
+}
+
+void ascending_order(int array[10])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int i = 0; i <9; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                int swap = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = swap;
+            }
+        }
+    }
+}
+
+void descending_order(int array[10])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int i = 0; i <9; i++)
+        {
+            if (array[i] < array[i + 1])
+            {
+                int swap = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = swap;
+            }
+        }
+    }
+}
+
+void print(int array[10])
+{
+    cout << "[";
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 9)
+        {
+            cout << array[i];
+        }
+        else {
+            cout << array[i] << ", ";
+        }
+    }
+    cout << "]" << endl;
+    s = array[1];
+}
+
 int main()
-{   
-    int n,number;
-    cout<<"Set the size of the array: ";
-    cin>>n;
-    int array[n];
-
-    for(int i=0;i<n;i++){
-      cout<<"Enter the "<<i+1<<". number: ";
-      cin>>number;
-     array[i]=number;        
-    }
-
-    cout<<"\nThe Array = [ ";
-
-    for(int j=0;j<n;j++){
-      cout<<array[j]<<" ";
-    }
-    cout<<"]";
+{
+    int A[10];
+    generate(A);
+    cout<<"\nAscended array A";
+    ascending_order(A);
+    print(A);
+    cout << "Second smallest number: " << s <<endl;
+    cout<<"\nDescended array A";
+    descending_order(A);
+    print(A);
+    cout << "Second highest number: " << s <<endl;
     return 0;
 }
